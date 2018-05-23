b29cf3d057f2e3367ef3bf2cb07fbf5587ec2613
Hernán Giraldo <hernan.giraldo.giraldo@gmail.com>
2018-05-23 14:14:33 -0400

    Day 6

---
+++ Day 6 - Ajax Type Ahead/script.js
@@ -0,0 +1,43 @@
+const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+let searchInput = document.querySelector('.search');
+let suggestions ;
+
+let cities = [];
+
+const prom = fetch(endpoint)
+    .then( blob => blob.json() )
+    .then( data => cities = data );
+
+function findMatches(wordToMatch) {
+    return cities.filter( place => {
+        const regex = new RegExp(wordToMatch, 'gi');
+        return place.city.match(regex) || place.state.match(regex);
+    });
+}
+
+function displayMatches() {
+    const matchArray = findMatches(this.value);
+    const html = matchArray.map( place => {
+        const regex = new RegExp(this.value, 'gi');
+        const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
+        const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`);
+        return `
+            <li>
+                <span class="name">${cityName}, ${stateName}</span>
+                <span class="population">${place.population}</span>
+            </li>
+        `;
+    }).join('');
+
+    suggestions.innerHTML = html;
+}
+
+const domLoaded = () => {
+    searchInput = document.querySelector('.search');
+    suggestions = document.querySelector('.suggestions');
+
+    searchInput.addEventListener('change', displayMatches)
+    searchInput.addEventListener('keyup', displayMatches)
+}
+
+document.addEventListener('DOMContentLoaded', domLoaded);
