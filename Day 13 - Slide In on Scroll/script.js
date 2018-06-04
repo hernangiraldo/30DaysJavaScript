836b82080e9ba74566bd9644dad6b1e345ed59c8
Hernán Giraldo <hernan.giraldo.giraldo@gmail.com>
2018-06-03 23:10:47 -0400

    Day 13

--- Day 13 - Slide In on Scroll/script.js
+++ Day 13 - Slide In on Scroll/script.js
@@ -1,11 +1,41 @@
-const pressed = [];
-const secretCode = 'webos';
+let sliderImages;
 
-window.addEventListener('keyup', (e) => {
-  pressed.push( e.key );
-  pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
-  
-  if ( pressed.join('').includes(secretCode) ) {
-    cornify_add();
-  }
-});
+const debounce = (func, wait = 20, immediate = true) => {
+  let timeout;
+  return function () {
+    let context = this;
+    let args = arguments;
+
+    let later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+function checkSlide(e) {
+  sliderImages.forEach(image => {
+    const slideInAt = (window.scrollY + window.innerHeight);
+    const imageBottom = image.offsetTop + image.height;
+    const isHalfShown = slideInAt > image.offsetTop;
+    const isNotScrolledPast = window.screenY < imageBottom;
+
+    if (isHalfShown && isNotScrolledPast) {
+      image.classList.add('active');
+    } else {
+      image.classList.remove('active');
+    }
+  });
+}
+
+const domLoaded = () => {
+  sliderImages = document.querySelectorAll('.slide-in');
+};
+
+window.addEventListener('scroll', debounce(checkSlide));
+document.addEventListener('DOMContentLoaded', domLoaded);
