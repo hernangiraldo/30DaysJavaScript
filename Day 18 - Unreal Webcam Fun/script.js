3ebb1b587aadbc2b8354d7bd0ed591ba7a08ac4a
Hernán Giraldo <hernan.giraldo.giraldo@gmail.com>
2018-07-02 18:45:31 -0400

    Funny camera

--- Day 18 - Unreal Webcam Fun/script.js
+++ Day 18 - Unreal Webcam Fun/script.js
@@ -5,20 +5,54 @@ let snap;
 let ctx;
 
 function getVideo() {
-    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-        .then( localMediaStream => {
+    navigator.mediaDevices.getUserMedia({
+            video: true,
+            audio: false
+        })
+        .then(localMediaStream => {
             video.src = window.URL.createObjectURL(localMediaStream);
             video.play();
-        }). catch( err => {
+        }).catch(err => {
             console.log('Nooooo!!!', err);
         });
 }
 
 function redEffect(pixels) {
-    for(let i=0 ; i < pixels.data.length; i+=4) {
-        pixels[i + 0] = pixels.data[i + 0] + 100;
-        pixels[i + 1] = pixels.data[i + 1] - 50;
-        pixels[i + 2] = pixels.data[i + 2] * 0.5;
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        pixels.data[i + 0] = pixels.data[i + 0] + 100;
+        pixels.data[i + 1] = pixels.data[i + 1] - 50;
+        pixels.data[i + 2] = pixels.data[i + 2] * 0.5;
+    }
+
+    return pixels;
+}
+
+function rgbSplit(pixels) {
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        pixels.data[i - 150] = pixels.data[i + 0];
+        pixels.data[i + 100] = pixels.data[i + 1];
+        pixels.data[i - 150] = pixels.data[i + 2];
+    }
+
+    return pixels;
+}
+
+function greenScreen(pixels) {
+    const levels = {};
+
+    document.querySelectorAll('.rgb input').forEach( (input) => {
+        levels[input.name] = input.value
+    });
+
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        red = pixels.data[i + 0];
+        green = pixels.data[i + 1];
+        blue = pixels.data[i + 2];
+        alpha = pixels.data[i + 3];
+
+        if (red >= levels.rmin && red >= levels.rmax && blue >= levels.rmin && blue >= levels.rmax && green >= levels.rmin && green >= levels.rmax) {
+            pixels.data[i + 3] = 0;
+        }
     }
 
     return pixels;
@@ -30,10 +64,11 @@ function paintToCanvas() {
     canvas.width = width;
     canvas.height = height;
 
-    return setInterval( () => {
+    return setInterval(() => {
         ctx.drawImage(video, 0, 0, width, height);
         let pixels = ctx.getImageData(0, 0, width, height);
-        pixels = redEffect(pixels);
+        pixels = greenScreen(pixels);
+        // ctx.globalAlpha = 0.1;
         ctx.putImageData(pixels, 0, 0);
     }, 16);
 }
